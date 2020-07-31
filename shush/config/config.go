e38d9b47e6187a0ddddd6f34b91f1fa18fe51a61
Vit Listik <tivvit@seznam.cz>
2020-08-01 00:12:26 +0200

    no need for flags

--- shush/config/config.go
+++ shush/config/config.go
@@ -66,25 +66,6 @@ func NewConf(fn string) (*Conf, error) {
 	viper.SetEnvPrefix("shush")
 	viper.AutomaticEnv()
 
-	// todo support flags?
-	//var cmd = &cobra.Command{
-	//	Use:   "viper-test",
-	//	Short: "testing viper",
-	//	Run: func(command *cobra.Command, args []string) {
-	//		fmt.Printf("thing1: %q\n", viper.GetString("thing1"))
-	//		fmt.Printf("thing2: %q\n", viper.GetString("thing2"))
-	//	},
-	//}
-	//
-	//viper.AutomaticEnv()
-	//flags := cmd.Flags()
-	//flags.String("thing1", "", "The first thing")
-	//viper.BindPFlag("thing1", flags.Lookup("thing1"))
-	//flags.String("thing2", "", "The second thing")
-	//viper.BindPFlag("thing2", flags.Lookup("thing2"))
-	//
-	//cmd.Execute()
-
 	var conf Conf
 
 	err := viper.Unmarshal(&conf)
